02cb94a2e0aaf5c452f1bcc0da04d8d3238e8938
Nshimyumukiza Jean de Dieu <jeandedieu2030@gmail.com>
2026-05-13 15:13:12 +0200

    Refactor ecosystem configuration for consistency and add sharp as a built dependency in pnpm lockfile

--- frontend/ecosystem.config.js
+++ frontend/ecosystem.config.js
@@ -1,11 +1,11 @@
 module.exports = {
   apps: [
     {
-      name: 'kickcraft-frontend',
-      script: 'node_modules/.bin/next',
-      args: 'start -p 5005',
+      name: "kickcraft-frontend",
+      script: "node_modules/.bin/next",
+      args: "start -p 5005",
       env: {
-        NODE_ENV: 'production',
+        NODE_ENV: "production",
       },
       instances: 1,
       autorestart: true,
